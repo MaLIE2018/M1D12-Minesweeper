d9eb6ff24032ad79f5918980face3581715ad671
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-23 13:58:25 +0100

    little bit of Bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,98 @@
 const board = document.getElementById("board")
-console.log('board:', board)
+const container = document.querySelector(".container")
 
-const FillArray = (count) => {
-    for (let i = 0; i < count; i++) {
-        userfieldNumbers.push(i)
+let numOfMines = 10
+let mines = Array(numOfMines)
+
+let availableBoardIndices = []
+let colPerRow = 9,
+    RowPerBoard = 9
+
+let numOfFields = colPerRow * RowPerBoard
+
+
+const FillArray = () => {
+    for (let i = 0; i < numOfFields; i++) {
+        availableBoardIndices.push(i)
+    }
+}
+FillArray()
+
+
+
+
+
+// Create the board
+const fillMinesweeperBoard = () => {
+    for (let i = 0; i < RowPerBoard; i++) {
+        let row = document.createElement("div")
+        row.classList.add("row")
+        for (let j = 0; j < colPerRow; j++) {
+            let col = document.createElement("div")
+            col.classList.add("col")
+            col.addEventListener("click", (event) => {
+                console.log(event.currentTarget.parentElement)
+            })
+            let span = document.createElement("span")
+            span.innerText = `rInd ${j} cInd ${i}`
+            span.classList.add("field")
+            span.classList.add("covered")
+            col.appendChild(span)
+            row.appendChild(col)
+        }
+        container.appendChild(row)
     }
 }
 
+fillMinesweeperBoard()
 
-const fillBingoField = (numOfFields) => {
-    for (let i = 1; i <= numOfFields; i++) {
-        let field = document.createElement("div")
-        let span = document.createElement("span")
 
-        field.appendChild(span).innerText = i
-        field.classList.add("field")
-        field.classList.add("covered")
-        board.appendChild(field)
-            //availableNumber.push(i)
+// Adding mines to the field and make sure they are distinct from one another
+const avoidRepetition = () => {
+    let stillAvailable = true
+    let pos = false
+    let randomNum = 0
+    do {
+        randomNum = Math.floor(Math.random() * numOfFields + 1)
+        pos = availableBoardIndices
+            .includes(randomNum)
+        if (pos === true) {
+            availableBoardIndices
+                .splice(availableBoardIndices
+                    .indexOf(randomNum), 1)
+            stillAvailable = false
+        }
+    } while (stillAvailable);
+    return randomNum
+}
 
+const addMines = () => {
+    const fields = document.getElementsByClassName("field covered")
+
+    for (let i = 0; i < numOfMines; i++) {
+        let index = avoidRepetition()
+        fields[index].innerText = "M"
+        fields[index].classList.add("mine")
+        mines[i] = index
     }
 }
 
-fillBingoField(81)
+addMines()
+
+const getNeighbors = (index) => {
+    let lsRInd = 0
+    let rsRind = colPerRow - 1
+    let tsCind = 0
+    let bsCind = RowPerBoard - 1
+    let rows = container.children
+    let col = rows[0].children[0]
+    console.log('col00:', col)
+
+}
+getNeighbors(0)
+
+window.onload = () => {
+
+
+
+}
