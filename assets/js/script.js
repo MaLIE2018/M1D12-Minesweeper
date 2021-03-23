8d4a106058aedb20ecd13423bc21efaeb835d265
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-23 22:53:32 +0100

    Fields are filled correctly with mines and numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const board = document.getElementById("board")
-const container = document.querySelector(".container")
+const container = document.querySelector(".container-fluid")
 
 let numOfMines = 10
 let mines = Array(numOfMines)
@@ -18,36 +18,62 @@ const FillArray = () => {
 }
 FillArray()
 
-
-
-
-
 // Create the board
 const fillMinesweeperBoard = () => {
-    for (let i = 0; i < RowPerBoard; i++) {
+    for (let rI = 0; rI < RowPerBoard; rI++) {
         let row = document.createElement("div")
         row.classList.add("row")
-        for (let j = 0; j < colPerRow; j++) {
+        for (let cI = 0; cI < colPerRow; cI++) {
             let col = document.createElement("div")
-            col.classList.add("col")
+            col.classList.add("col", "col-sm", "col-md", "col-lg", "col-xl", "m-2")
+            col.id = `${rI}${cI}`
             col.addEventListener("click", (event) => {
-                console.log(event.currentTarget.parentElement)
+                //getNeighbors(event.currentTarget.id)
             })
-            let span = document.createElement("span")
-            span.innerText = `rInd ${j} cInd ${i}`
-            span.classList.add("field")
-            span.classList.add("covered")
-            col.appendChild(span)
+            let div = document.createElement("div")
+            div.innerText = "" //`rInd ${rI} cInd ${cI}`
+            div.classList.add("field", "covered", "p-2")
+            col.appendChild(div)
             row.appendChild(col)
         }
         container.appendChild(row)
     }
 }
-
 fillMinesweeperBoard()
 
 
-// Adding mines to the field and make sure they are distinct from one another
+// Adding mines to the field and make sure they are distinct from one another.
+// Calculating the field values for the direct neighbors of a mine.
+const calcSumOfNumberfields = (id) => {
+    id = id.split("")
+    rI = parseInt(id[0])
+    cI = parseInt(id[1])
+
+    neighbors = [
+        `${rI- 1}${cI - 1}`, //topleft
+        `${rI- 1}${cI}`, //top
+        `${rI- 1}${cI + 1}`, //topright
+        `${rI}${cI -1}`, //left
+        `${rI}${cI + 1}`, //right
+        `${rI+ 1}${cI - 1}`, //bottomleft
+        `${rI+ 1}${cI}`, //bottom
+        `${rI+ 1}${cI + 1}`, //bottomright
+    ]
+
+    for (const neighbor of neighbors) {
+        let currentEle = document.getElementById(neighbor)
+        let div = ""
+        if (currentEle !== null && currentEle.firstChild.classList.contains("mine") === false) {
+            div = currentEle.firstChild
+            if (div.innerText === "") {
+                div.innerText = "1"
+            } else {
+                div.innerText = parseInt(div.innerText) + 1
+            }
+        }
+    }
+}
+
 const avoidRepetition = () => {
     let stillAvailable = true
     let pos = false
@@ -70,26 +96,22 @@ const addMines = () => {
     const fields = document.getElementsByClassName("field covered")
 
     for (let i = 0; i < numOfMines; i++) {
-        let index = avoidRepetition()
+        let index = avoidRepetition() //create random mine
         fields[index].innerText = "M"
-        fields[index].classList.add("mine")
-        mines[i] = index
+        fields[index].classList.add("mine") //marked as a mine
+
+        //Increment the neighbor's field values by one. 
+        calcSumOfNumberfields(fields[index].parentElement.id)
+            //Id's of the col is saved in the mines array
+        mines[i] = fields[index].parentElement.id
+            //console.log('mines:', mines)
     }
 }
 
 addMines()
 
-const getNeighbors = (index) => {
-    let lsRInd = 0
-    let rsRind = colPerRow - 1
-    let tsCind = 0
-    let bsCind = RowPerBoard - 1
-    let rows = container.children
-    let col = rows[0].children[0]
-    console.log('col00:', col)
 
-}
-getNeighbors(0)
+
 
 window.onload = () => {
 
