dbfce443ee53136215430a3f79cd5de95ce883b7
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-25 22:38:19 +0100

    allmost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,21 +18,96 @@ const FillArray = () => {
 }
 FillArray()
 
+// Gameover
+const gameOver = () => {
+    alert("You losed")
+}
+
+
+
+const uncoverFields = (id) => {
+    id = id.split("")
+    let indr = parseInt(id[0])
+    let indc = parseInt(id[1])
+
+
+    neighbors = [
+        `${indr- 1}${indc - 1}`, //topleft
+        `${indr- 1}${indc}`, //top
+        `${indr- 1}${indc + 1}`, //topindrght
+        `${indr}${indc -1}`, //left
+        `${indr}${indc + 1}`, //indrght
+        `${indr+ 1}${indc - 1}`, //bottomleft
+        `${indr+ 1}${indc}`, //bottom
+        `${indr+ 1}${indc + 1}`, //bottomindrght
+    ]
+
+    for (const neigh of neighbors) {
+        let currentEle = document.getElementById(neigh)
+        console.log('currentEle:', currentEle)
+        if (currentEle !== null && currentEle.classList.contains("mine") === false) {
+            let mine = currentEle.classList.contains("mine")
+            let num = currentEle.classList.contains("num")
+            if (num) {
+                currentEle.classList.remove("covered")
+                currentEle.classList.add("uncovered")
+                currentEle.firstChild.classList.remove("covered")
+                currentEle.firstChild.classList.add("uncovered")
+            } else if (!num && !mine && currentEle.classList.contains("uncovered") == false) {
+                currentEle.classList.remove("covered")
+                currentEle.firstChild.classList.remove("covered")
+                currentEle.firstChild.classList.add("uncovered")
+                currentEle.classList.add("uncovered")
+                uncoverFields(currentEle.id)
+            }
+        }
+    }
+}
+
+//Which fields should be uncovered
+
 // Create the board
 const fillMinesweeperBoard = () => {
-    for (let rI = 0; rI < RowPerBoard; rI++) {
+    for (let indr = 0; indr < RowPerBoard; indr++) {
         let row = document.createElement("div")
         row.classList.add("row")
-        for (let cI = 0; cI < colPerRow; cI++) {
+        for (let indc = 0; indc < colPerRow; indc++) {
             let col = document.createElement("div")
-            col.classList.add("col", "col-sm", "col-md", "col-lg", "col-xl", "m-2")
-            col.id = `${rI}${cI}`
+            col.classList.add("col", "col-sm", "col-md", "col-lg", "col-xl", "m-2", "p-0", "covered")
+            col.id = `${indr}${indc}`
             col.addEventListener("click", (event) => {
-                //getNeighbors(event.currentTarget.id)
+                let mine = event.currentTarget.classList.contains("mine")
+                let num = event.currentTarget.classList.contains("num")
+                let covered = event.currentTarget.classList.contains("covered")
+                let flagged = event.currentTarget.classList.contains("flagged")
+
+                if (flagged) {
+                    event.currentTarget.querySelector("#flag").remove()
+                    event.currentTarget.classList.remove("#flag")
+                    countFlags()
+                } else {
+                    if (mine) {
+                        gameOver()
+                    }
+                    if (num && covered) {
+                        event.currentTarget.classList.add("uncovered")
+                        event.currentTarget.firstChild.classList.remove("covered")
+                        event.currentTarget.firstChild.classList.add("uncovered")
+                    } else if (num && !covered) {
+                        //lucky shot
+                    }
+                    if (!num && !mine) {
+                        event.currentTarget.classList.add("covered")
+                        event.currentTarget.classList.add("uncovered")
+                        uncoverFields(event.currentTarget.id)
+                    }
+                }
+
+
             })
             let div = document.createElement("div")
-            div.innerText = "" //`rInd ${rI} cInd ${cI}`
-            div.classList.add("field", "covered", "p-2")
+            div.innerText = "" //`indrnd ${indr} indcnd ${indc}`
+            div.classList.add("field", "covered")
             col.appendChild(div)
             row.appendChild(col)
         }
@@ -46,30 +121,31 @@ fillMinesweeperBoard()
 // Calculating the field values for the direct neighbors of a mine.
 const calcSumOfNumberfields = (id) => {
     id = id.split("")
-    rI = parseInt(id[0])
-    cI = parseInt(id[1])
+    indr = parseInt(id[0])
+    indc = parseInt(id[1])
 
     neighbors = [
-        `${rI- 1}${cI - 1}`, //topleft
-        `${rI- 1}${cI}`, //top
-        `${rI- 1}${cI + 1}`, //topright
-        `${rI}${cI -1}`, //left
-        `${rI}${cI + 1}`, //right
-        `${rI+ 1}${cI - 1}`, //bottomleft
-        `${rI+ 1}${cI}`, //bottom
-        `${rI+ 1}${cI + 1}`, //bottomright
+        `${indr- 1}${indc - 1}`, //topleft
+        `${indr- 1}${indc}`, //top
+        `${indr- 1}${indc + 1}`, //topindrght
+        `${indr}${indc -1}`, //left
+        `${indr}${indc + 1}`, //indrght
+        `${indr+ 1}${indc - 1}`, //bottomleft
+        `${indr+ 1}${indc}`, //bottom
+        `${indr+ 1}${indc + 1}`, //bottomindrght
     ]
 
     for (const neighbor of neighbors) {
         let currentEle = document.getElementById(neighbor)
         let div = ""
-        if (currentEle !== null && currentEle.firstChild.classList.contains("mine") === false) {
+        if (currentEle !== null && currentEle.classList.contains("mine") === false) {
             div = currentEle.firstChild
             if (div.innerText === "") {
                 div.innerText = "1"
             } else {
                 div.innerText = parseInt(div.innerText) + 1
             }
+            currentEle.classList.add("num")
         }
     }
 }
@@ -98,23 +174,40 @@ const addMines = () => {
     for (let i = 0; i < numOfMines; i++) {
         let index = avoidRepetition() //create random mine
         fields[index].innerText = "M"
-        fields[index].classList.add("mine") //marked as a mine
+        fields[index].parentElement.classList.add("mine") //marked as a mine
 
         //Increment the neighbor's field values by one. 
         calcSumOfNumberfields(fields[index].parentElement.id)
             //Id's of the col is saved in the mines array
         mines[i] = fields[index].parentElement.id
-            //console.log('mines:', mines)
     }
 }
 
 addMines()
 
+const setAFlag = (event) => {
+    let fields = event.path
+    let flag = `<svg xmlns="http://www.w3.org/2000/svg" id="flag" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
+    <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
+  </svg>`
+    for (const item of fields) {
+        if (item.classList.contains("col") === true) {
+            item.classList.add("flagged")
+            item.innerHTML += flag
+            countFlags()
+        }
+    }
+}
 
+const countFlags = () => {
+    let resultFlag = document.getElementById("setFlags")
+    let result = numOfMines - parseInt(document.querySelectorAll("#flag").length)
+    console.log('result:', result)
+    resultFlag.innerText = result
+}
 
-
-window.onload = () => {
-
-
-
+//set a flag 
+window.oncontextmenu = function(event) {
+    setAFlag(event);
+    return false; // cancel default menu
 }
